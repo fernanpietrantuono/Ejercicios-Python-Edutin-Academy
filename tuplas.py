0ad70c6da7d68b342fa290999c986cf3c1b7031f
Fernán Pietrantuono <fernanpietrantuono@gmail.com>
2024-08-06 21:02:08 -0300

    Cuarto Commit

--- tuplas.py
+++ tuplas.py
@@ -4,8 +4,18 @@ cuántas veces se repite
 """
 numeros = (5, 6, 7, 8, 5, 5, 6, 90, 12, 14, 12)
 numero = int(input("Dame un número: "))
-if numeros.count(numero) == 1:
-    print(f'El número {numero} se repite {numeros.count(numero)} vez')
+indices = []
+if numero in numeros:
+    if numeros.count(numero) == 1:
+        print(f'El número {numero} se repite {numeros.count(numero)} vez en la tupla')
+    else:
+        print(f'El número {numero} se repite {numeros.count(numero)} veces en la tupla')
+    for i, valor in enumerate(numeros):
+        if valor == numero:
+            indices.append(i)
+    if len(indices) == 1:
+        print(f'El número {numero} se encuentra en el índice {indices} de la tupla')
+    else:
+        print(f'El número {numero} se encuentra en los índices {indices} de la tupla')
 else:
-    print(f'El número {numero} se repite {numeros.count(numero)} veces')
-print(f'El número {numero} se encuentra en el índice {numeros.index(numero)}')
+    print(f'Lo siento, el número {numero} no está en la tupla ☹')
